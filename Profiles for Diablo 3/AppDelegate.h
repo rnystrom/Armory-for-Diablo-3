af3f1b584713af0453c2a47de5e5ebcde2695270
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2012-08-15 09:02:14 -0400

    Initial Commit

---
+++ Profiles for Diablo 3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Profiles for Diablo 3
+//
+//  Created by Ryan Nystrom on 8/15/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
